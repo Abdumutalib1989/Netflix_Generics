3fb8d95a913d58d0ba8e609bc2496d827ca64581
Abdumutalib Mirzajonov <Abdumutalib1989>
2021-12-18 17:49:24 +0500

    Secret Key o'zgardi

--- Netflix2/settings.py
+++ Netflix2/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ["SECRET_KEY"]
+SECRET_KEY = 'django-insecure-frm2s)(9h+x)ve&zmi+ml1^-5t!-cfpgooq@^c9p4(t((=)qdx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
